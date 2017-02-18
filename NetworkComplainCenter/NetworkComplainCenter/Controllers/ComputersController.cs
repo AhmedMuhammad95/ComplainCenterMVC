3ae732b77688d347ea95ca7556d8e4558694abd9
AhmedMuhammad95 <Ahmedsayal95@gmail.com>
2017-02-18 23:08:35 +0500

    Uploading project

---
+++ NetworkComplainCenter/NetworkComplainCenter/Controllers/ComputersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetworkComplainCenter.Controllers
+{
+    public class ComputersController : Controller
+    {
+        //
+        // GET: /Computers/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
